8973030f048cf931c4e33010ffbde5a49f8a7be5
Lisa <lsr488@gmail.com>
2018-12-26 22:31:09 -0500

    Planning.

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// game grid is 4x4
+// begin with 2 tiles placed randomly, each with value of 2
+// all tiles move in the direction of the arrow press (it's NOT per tile)
+// if adjacent tiles in direction of movement, add values together
